f7afa4fd47aa63ce54416986171be34a791be13d
RedMarsel <31604969+RedMarsel@users.noreply.github.com>
2017-09-11 07:38:18 +0300

    Add files via upload

--- src/imit/student/Point3D.java
+++ src/imit/student/Point3D.java
@@ -1,5 +1,18 @@
 package imit.student;
 
 public class Point3D {
+    private double x;
+    private double y;
+    private double z;
+    public void constructCoordinate(double constructx, double constructy, double constructz) {
+        x=constructx;
+        y=constructy;
+        z=constructz;
+    }
+    public void constructNoCoordinate() {
+        x=0;
+        y=0;
+        z=0;
+    }
 
 }
